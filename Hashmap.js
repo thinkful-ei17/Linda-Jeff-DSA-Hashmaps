521e582a98767895c73b83a2b2cff73d528fdd34
Jeff Connelly <jeff.connelly6@gmail.com>
2018-03-05 09:30:35 -0700

    Initializing repo

---
+++ Hashmap.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+class HashMap {
+  constructor(initialCapacity=8) {
+    this.length = 0;
+    this._slots = [];
+    this._capacity = initialCapacity;
+    this._deleted = 0;
+  }
+
+
+
+}
